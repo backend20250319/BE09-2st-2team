a2aff4512d5d9c5881c66ca6eb188db4c778ca39
junga970 <junga2940@gmail.com>
2025-05-15 17:59:17 +0900

    로그인 페이지 1차 구현

--- src/app/(features)/auth/sighup/page.jsx
+++ src/app/(features)/auth/sighup/page.jsx
@@ -2,7 +2,7 @@ import LoginForm from "../components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <main style={{ padding: "20px" }}>
+    <main>
       <h1>로그인 페이지</h1>
       <LoginForm />
     </main>
