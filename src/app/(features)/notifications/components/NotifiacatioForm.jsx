ab800640556c592490da0cd5f791c2d21bd08703
hyunwoo_lim <dla6963@naver.com>
2025-05-15 16:50:34 +0900

    firstcommit

--- src/app/(features)/notifications/components/NotifiacatioForm.jsx
+++ src/app/(features)/notifications/components/NotifiacatioForm.jsx
@@ -1,16 +1,59 @@
 "use client";
+import "../style.css";
 
 export default function NotifiacationForm() {
   return (
-    <div>
-      <div className="container">
-        <div className="id">1</div>
+    <div className="noti-container">
+      <div>
+        <div
+          style={{
+            height: 30,
+            fontWeight: "bold",
+            fontSize: 20,
+          }}
+        >
+          알림
+        </div>
+        <div className="noti-item">
+          <img
+            src="public/images/noti/id1.profile.jpg"
+            className="noti-profile"
+          ></img>
+          <div className="noti-content"></div>
+          cooling_0님과 no.yoonseok님이 회원님의 스토리를 좋아합니다.
+        </div>
       </div>
-      <div className="container">
-        <div className="id">2</div>
+      <div>
+        <div
+          style={{
+            height: 30,
+            fontWeight: "bold",
+            fontSize: 15,
+            paddingTop: 15,
+          }}
+        >
+          이번 달
+        </div>
+        <div className="noti-item">알림</div>
+        <div className="noti-item">알림</div>
+        <div className="noti-item">요소 공간</div>
       </div>
-      <div className="container">
-        <div className="id">3</div>
+      <div>
+        <div
+          style={{
+            height: 30,
+            fontWeight: "bold",
+            fontSize: 15,
+            paddingTop: 15,
+          }}
+        >
+          이전 활동
+        </div>
+        <div className="noti-item">알림</div>
+        <div className="noti-item">요소 공간</div>
+        <div className="noti-item">요소 공간</div>
+        <div className="noti-item">요소 공간</div>
+        <div className="noti-item">요소 공간</div>
       </div>
     </div>
   );
--- src/app/(features)/notifications/layout.js
+++
@@ -1,3 +0,0 @@
-export default function Layout() {
-  return <h1>안녕하세요</h1>;
-}
