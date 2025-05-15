1145f4d9c6f925a7a1cecda12969a9fefd12f658
hyunwoo_lim <dla6963@naver.com>
2025-05-15 14:38:21 +0900

    add favicon

---
+++ src/app/(features)/notifications/components/NotifiacatioForm.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function NotifiacationForm() {
+  return (
+    <div>
+      <div className="container">
+        <div className="id">1</div>
+      </div>
+      <div className="container">
+        <div className="id">2</div>
+      </div>
+      <div className="container">
+        <div className="id">3</div>
+      </div>
+    </div>
+  );
+}
