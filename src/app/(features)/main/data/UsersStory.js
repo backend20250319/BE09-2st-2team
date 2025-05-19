62e081e47e3663118dfcd00c7754e390a93d7b0c
junseo <junseo9145@gmail.com>
2025-05-19 15:20:54 +0900

    이름 오류 해결

--- src/app/(features)/main/data/UsersStory.js
+++ src/app/(features)/main/data/UsersStory.js
@@ -1,8 +1,8 @@
 // storyUsers.js
 const storyUsers = [
-  { username: "maestro", imgSrc: "/images/search/profile9.png" },
+  { username: "maestro_guy", imgSrc: "/images/search/profile9.png" },
   { username: "ironMAN", imgSrc: "/images/profile/profile1.jpg" },
-  { username: "blueThings", imgSrc: "/images/search/profile2.png" },
+  { username: "purple_Things", imgSrc: "/images/search/profile2.png" },
   { username: "leeMunSeok", imgSrc: "/images/search/profile9.png" },
   { username: "firstV", imgSrc: "/images/search/profile3.png" },
   { username: "1__sky", imgSrc: "/images/search/profile1.png" },
