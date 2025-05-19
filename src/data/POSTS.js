4ef788e9e9c5f43bab91a59103af068758ad04b2
hyeblee <hyeb1224@naver.com>
2025-05-19 16:09:08 +0900

    feat: 프로필에 포스트 목록 추가를 위한 데이터 추가

--- src/data/POSTS.js
+++ src/data/POSTS.js
@@ -83,5 +83,349 @@ export const POSTS = [
       },
     ],
   },
+  {
+    id: 3,
+    images: [
+      "/images/post/post3/post3_1.jpg",
+      "/images/post/post2/post2_2.jpg",
+      "/images/post/post2/post2_3.jpg",
+      "/images/post/post2/post2_4.jpg",
+      "/images/post/post2/post2_5.jpg",
+      "/images/post/post2/post2_6.jpg",
+    ],
+    username: "playdata.io",
+    userAvatar: "/images/profile/profile1.jpg",
+    content: `👩🏻‍💻수료생 취업후기👩🏻‍💻
+
+정보통신공학 전공자에서 쇼핑몰 대표, 그리고 IT교육매니저까지! 원하는 것은 모두 이루어 내는 채영님의 취업 스토리를 확인해보세요.🥳
+
+“시야를 확장하니 더 많은 취업 기회가 생기더라고요. 그 결과 지금은 엔코아의 동료가 되어 함께 일하고 있어요!"
+
+👉🏻채영님 인터뷰 보러가기
+(프로필 링크 ✨취업후기✨ 클릭)`,
+    comments: [
+      {
+        username: "seungje_yu",
+        text: "열공해요!",
+        img: "/images/post/comment/seungje.png",
+      },
+      {
+        username: "LimHub",
+        text: "멋져요~",
+        img: "/images/post/comment/LimHub.png",
+      },
+      {
+        username: "Berry-mas",
+        text: "배고파요👿",
+        img: "/images/post/comment/Berry-mas.png",
+      },
+      {
+        username: "hyeblee",
+        text: "집에 가고 싶어요🏠",
+        img: "/images/post/comment/hyeblee.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    images: [
+      "/images/post/post3/post3_1.jpg",
+      "/images/post/post2/post2_2.jpg",
+      "/images/post/post2/post2_3.jpg",
+      "/images/post/post2/post2_4.jpg",
+      "/images/post/post2/post2_5.jpg",
+      "/images/post/post2/post2_6.jpg",
+    ],
+    username: "playdata.io",
+    userAvatar: "/images/profile/profile1.jpg",
+    content: `👩🏻‍💻수료생 취업후기👩🏻‍💻
+
+정보통신공학 전공자에서 쇼핑몰 대표, 그리고 IT교육매니저까지! 원하는 것은 모두 이루어 내는 채영님의 취업 스토리를 확인해보세요.🥳
+
+“시야를 확장하니 더 많은 취업 기회가 생기더라고요. 그 결과 지금은 엔코아의 동료가 되어 함께 일하고 있어요!"
+
+👉🏻채영님 인터뷰 보러가기
+(프로필 링크 ✨취업후기✨ 클릭)`,
+    comments: [
+      {
+        username: "seungje_yu",
+        text: "열공해요!",
+        img: "/images/post/comment/seungje.png",
+      },
+      {
+        username: "LimHub",
+        text: "멋져요~",
+        img: "/images/post/comment/LimHub.png",
+      },
+      {
+        username: "Berry-mas",
+        text: "배고파요👿",
+        img: "/images/post/comment/Berry-mas.png",
+      },
+      {
+        username: "hyeblee",
+        text: "집에 가고 싶어요🏠",
+        img: "/images/post/comment/hyeblee.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    images: [
+      "/images/post/post4/post4_1.jpg",
+      "/images/post/post2/post2_2.jpg",
+      "/images/post/post2/post2_3.jpg",
+      "/images/post/post2/post2_4.jpg",
+      "/images/post/post2/post2_5.jpg",
+      "/images/post/post2/post2_6.jpg",
+    ],
+    username: "playdata.io",
+    userAvatar: "/images/profile/profile1.jpg",
+    content: `👩🏻‍💻수료생 취업후기👩🏻‍💻
+
+정보통신공학 전공자에서 쇼핑몰 대표, 그리고 IT교육매니저까지! 원하는 것은 모두 이루어 내는 채영님의 취업 스토리를 확인해보세요.🥳
+
+“시야를 확장하니 더 많은 취업 기회가 생기더라고요. 그 결과 지금은 엔코아의 동료가 되어 함께 일하고 있어요!"
+
+👉🏻채영님 인터뷰 보러가기
+(프로필 링크 ✨취업후기✨ 클릭)`,
+    comments: [
+      {
+        username: "seungje_yu",
+        text: "열공해요!",
+        img: "/images/post/comment/seungje.png",
+      },
+      {
+        username: "LimHub",
+        text: "멋져요~",
+        img: "/images/post/comment/LimHub.png",
+      },
+      {
+        username: "Berry-mas",
+        text: "배고파요👿",
+        img: "/images/post/comment/Berry-mas.png",
+      },
+      {
+        username: "hyeblee",
+        text: "집에 가고 싶어요🏠",
+        img: "/images/post/comment/hyeblee.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    images: [
+      "/images/post/post5/post5_1.jpg",
+      "/images/post/post2/post2_2.jpg",
+      "/images/post/post2/post2_3.jpg",
+      "/images/post/post2/post2_4.jpg",
+      "/images/post/post2/post2_5.jpg",
+      "/images/post/post2/post2_6.jpg",
+    ],
+    username: "playdata.io",
+    userAvatar: "/images/profile/profile1.jpg",
+    content: `👩🏻‍💻수료생 취업후기👩🏻‍💻
+
+정보통신공학 전공자에서 쇼핑몰 대표, 그리고 IT교육매니저까지! 원하는 것은 모두 이루어 내는 채영님의 취업 스토리를 확인해보세요.🥳
+
+“시야를 확장하니 더 많은 취업 기회가 생기더라고요. 그 결과 지금은 엔코아의 동료가 되어 함께 일하고 있어요!"
+
+👉🏻채영님 인터뷰 보러가기
+(프로필 링크 ✨취업후기✨ 클릭)`,
+    comments: [
+      {
+        username: "seungje_yu",
+        text: "열공해요!",
+        img: "/images/post/comment/seungje.png",
+      },
+      {
+        username: "LimHub",
+        text: "멋져요~",
+        img: "/images/post/comment/LimHub.png",
+      },
+      {
+        username: "Berry-mas",
+        text: "배고파요👿",
+        img: "/images/post/comment/Berry-mas.png",
+      },
+      {
+        username: "hyeblee",
+        text: "집에 가고 싶어요🏠",
+        img: "/images/post/comment/hyeblee.png",
+      },
+    ],
+  },
+  {
+    id: 6,
+    images: [
+      "/images/post/post6/post6_1.jpg",
+      "/images/post/post2/post2_2.jpg",
+      "/images/post/post2/post2_3.jpg",
+      "/images/post/post2/post2_4.jpg",
+      "/images/post/post2/post2_5.jpg",
+      "/images/post/post2/post2_6.jpg",
+    ],
+    username: "playdata.io",
+    userAvatar: "/images/profile/profile1.jpg",
+    content: `👩🏻‍💻수료생 취업후기👩🏻‍💻
+
+정보통신공학 전공자에서 쇼핑몰 대표, 그리고 IT교육매니저까지! 원하는 것은 모두 이루어 내는 채영님의 취업 스토리를 확인해보세요.🥳
+
+“시야를 확장하니 더 많은 취업 기회가 생기더라고요. 그 결과 지금은 엔코아의 동료가 되어 함께 일하고 있어요!"
+
+👉🏻채영님 인터뷰 보러가기
+(프로필 링크 ✨취업후기✨ 클릭)`,
+    comments: [
+      {
+        username: "seungje_yu",
+        text: "열공해요!",
+        img: "/images/post/comment/seungje.png",
+      },
+      {
+        username: "LimHub",
+        text: "멋져요~",
+        img: "/images/post/comment/LimHub.png",
+      },
+      {
+        username: "Berry-mas",
+        text: "배고파요👿",
+        img: "/images/post/comment/Berry-mas.png",
+      },
+      {
+        username: "hyeblee",
+        text: "집에 가고 싶어요🏠",
+        img: "/images/post/comment/hyeblee.png",
+      },
+    ],
+  },
+  {
+    id: 7,
+    images: [
+      "/images/post/post7/post7_1.jpg",
+      "/images/post/post2/post2_2.jpg",
+      "/images/post/post2/post2_3.jpg",
+      "/images/post/post2/post2_4.jpg",
+      "/images/post/post2/post2_5.jpg",
+      "/images/post/post2/post2_6.jpg",
+    ],
+    username: "playdata.io",
+    userAvatar: "/images/profile/profile1.jpg",
+    content: `👩🏻‍💻수료생 취업후기👩🏻‍💻
+
+정보통신공학 전공자에서 쇼핑몰 대표, 그리고 IT교육매니저까지! 원하는 것은 모두 이루어 내는 채영님의 취업 스토리를 확인해보세요.🥳
+
+“시야를 확장하니 더 많은 취업 기회가 생기더라고요. 그 결과 지금은 엔코아의 동료가 되어 함께 일하고 있어요!"
+
+👉🏻채영님 인터뷰 보러가기
+(프로필 링크 ✨취업후기✨ 클릭)`,
+    comments: [
+      {
+        username: "seungje_yu",
+        text: "열공해요!",
+        img: "/images/post/comment/seungje.png",
+      },
+      {
+        username: "LimHub",
+        text: "멋져요~",
+        img: "/images/post/comment/LimHub.png",
+      },
+      {
+        username: "Berry-mas",
+        text: "배고파요👿",
+        img: "/images/post/comment/Berry-mas.png",
+      },
+      {
+        username: "hyeblee",
+        text: "집에 가고 싶어요🏠",
+        img: "/images/post/comment/hyeblee.png",
+      },
+    ],
+  },
+  {
+    id: 8,
+    images: [
+      "/images/post/post8/post8_1.jpg",
+      "/images/post/post2/post2_2.jpg",
+      "/images/post/post2/post2_3.jpg",
+      "/images/post/post2/post2_4.jpg",
+      "/images/post/post2/post2_5.jpg",
+      "/images/post/post2/post2_6.jpg",
+    ],
+    username: "playdata.io",
+    userAvatar: "/images/profile/profile1.jpg",
+    content: `👩🏻‍💻수료생 취업후기👩🏻‍💻
+
+정보통신공학 전공자에서 쇼핑몰 대표, 그리고 IT교육매니저까지! 원하는 것은 모두 이루어 내는 채영님의 취업 스토리를 확인해보세요.🥳
+
+“시야를 확장하니 더 많은 취업 기회가 생기더라고요. 그 결과 지금은 엔코아의 동료가 되어 함께 일하고 있어요!"
+
+👉🏻채영님 인터뷰 보러가기
+(프로필 링크 ✨취업후기✨ 클릭)`,
+    comments: [
+      {
+        username: "seungje_yu",
+        text: "열공해요!",
+        img: "/images/post/comment/seungje.png",
+      },
+      {
+        username: "LimHub",
+        text: "멋져요~",
+        img: "/images/post/comment/LimHub.png",
+      },
+      {
+        username: "Berry-mas",
+        text: "배고파요👿",
+        img: "/images/post/comment/Berry-mas.png",
+      },
+      {
+        username: "hyeblee",
+        text: "집에 가고 싶어요🏠",
+        img: "/images/post/comment/hyeblee.png",
+      },
+    ],
+  },
+  {
+    id: 9,
+    images: [
+      "/images/post/post9/post9_1.jpg",
+      "/images/post/post2/post2_2.jpg",
+      "/images/post/post2/post2_3.jpg",
+      "/images/post/post2/post2_4.jpg",
+      "/images/post/post2/post2_5.jpg",
+      "/images/post/post2/post2_6.jpg",
+    ],
+    username: "playdata.io",
+    userAvatar: "/images/profile/profile1.jpg",
+    content: `👩🏻‍💻수료생 취업후기👩🏻‍💻
+
+정보통신공학 전공자에서 쇼핑몰 대표, 그리고 IT교육매니저까지! 원하는 것은 모두 이루어 내는 채영님의 취업 스토리를 확인해보세요.🥳
+
+“시야를 확장하니 더 많은 취업 기회가 생기더라고요. 그 결과 지금은 엔코아의 동료가 되어 함께 일하고 있어요!"
+
+👉🏻채영님 인터뷰 보러가기
+(프로필 링크 ✨취업후기✨ 클릭)`,
+    comments: [
+      {
+        username: "seungje_yu",
+        text: "열공해요!",
+        img: "/images/post/comment/seungje.png",
+      },
+      {
+        username: "LimHub",
+        text: "멋져요~",
+        img: "/images/post/comment/LimHub.png",
+      },
+      {
+        username: "Berry-mas",
+        text: "배고파요👿",
+        img: "/images/post/comment/Berry-mas.png",
+      },
+      {
+        username: "hyeblee",
+        text: "집에 가고 싶어요🏠",
+        img: "/images/post/comment/hyeblee.png",
+      },
+    ],
+  },
   // 여기에 post 2, 3... 더 추가 가능
 ];
