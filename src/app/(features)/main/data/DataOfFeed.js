d654127427c04c45f72f3b0bcb156b89445e1f71
junseo <junseo9145@gmail.com>
2025-05-19 15:58:19 +0900

    발표 직전

--- src/app/(features)/main/data/DataOfFeed.js
+++ src/app/(features)/main/data/DataOfFeed.js
@@ -5,60 +5,73 @@ const FeedData = [
     username: "juns2o",
     profileImgUrl: "/images/main/main1_1.jpg",
     imgUrl: "/images/post/post1/post1_1.jpg",
-    content: "안녕하세요, 박준서입니다.",
+    content: `"부트캠프 동기들과 책 스터디를 했는데 최종 면접에서 저희가 읽은 책에 관심을 가져주셨어요. 
+            오프라인 부트캠프 정말 좋았어요. 사람은 사람을 만나야 한다는 생각이 들어요💓"
+            
+백엔드 개발 부트캠프를 수료하고 풀스택 개발자로 취업한 채림님의 풀 스토리를 확인해보세요!
+
+채림님 취업 스토리 확인하기 👉🏻 프로필 링크 클릭`,
   },
   {
     username: "lg_twins_fighting",
     profileImgUrl: "/images/main/main1_1.jpg",
-    imgUrl: "/images/feed/feed1/feed1_2.jpg",
+    imgUrl: "/images/post/post1/post1_2.jpg",
     content: "LG Twins 파이팅❤",
   },
   {
     username: "ys_politics",
     profileImgUrl: "/images/main/main1_1.jpg",
-    imgUrl: "/images/feed/feed1/feed1_3.jpg",
+    imgUrl: "/images/post/post1/post1_3.jpg",
+
     content: "국민의, 국민에 의한, 국민을 위한 정치",
   },
   {
     username: "statistics_dataScience",
     profileImgUrl: "/images/main/main1_1.jpg",
-    imgUrl: "/images/feed/feed1/feed1_4.jpg",
+    imgUrl: "/images/post/post1/post1_4.jpg",
+
     content: "우리 같이 적분하지 않을래?",
   },
   {
     username: "mlb_baseballNews",
     profileImgUrl: "/images/main/main1_1.jpg",
-    imgUrl: "/images/feed/feed2/feed2_1.jpg",
+    imgUrl: "/images/post/post1/post1_5.jpg",
+
     content: "오늘 김혜성 선수가 mlb에서 뛰어난 활약을 펼쳤습니다.",
   },
   {
     username: "princess",
     profileImgUrl: "/images/main/main1_1.jpg",
-    imgUrl: "/images/feed/feed2/feed2_2.jpg",
+    imgUrl: "/images/post/post1/post1_6.jpg",
+
     content: "난 원령공주, 넌 원령왕자",
   },
   {
     username: "prince_OH",
     profileImgUrl: "/images/main/main1_1.jpg",
-    imgUrl: "/images/feed/feed2/feed2_3.jpg",
+    imgUrl: "/images/post/post1/post2_1.jpg",
+
     content: "만나서 반갑습니다. 엘지 오지환입니다.",
   },
   {
     username: "dust_star_cccandy",
     profileImgUrl: "/images/main/main1_1.jpg",
-    imgUrl: "/images/feed/feed2/feed2_4.jpg",
+    imgUrl: "/images/post/post2/post2_2.png",
+
     content: "난 이~~따만큼 큰 별사탕 먹을 수 있다?!.",
   },
   {
     username: "harryPotter",
     profileImgUrl: "/images/main/main1_1.jpg",
-    imgUrl: "/images/feed/feed2/feed2_5.jpg",
+    imgUrl: "/images/post/post2/post2_3.png",
+
     content: "입닥쳐 말포이",
   },
   {
     username: "ys_politics",
     profileImgUrl: "/images/main/main1_1.jpg",
-    imgUrl: "/images/feed/feed2/feed2_6.jpg",
+    imgUrl: "/images/post/post2/post2_4.png",
+
     content: "헌법 제1조 1항: 대한민국은 민주공화국이다.",
   },
 ];
