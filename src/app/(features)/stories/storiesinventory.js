aefd5f542c75fa1f291a95b6de602230eea1ea5f
hyunwoo_lim <dla6963@naver.com>
2025-05-19 14:53:43 +0900

    디렉토리명최종

--- src/app/(features)/stories/storiesinventory.js
+++ src/app/(features)/stories/storiesinventory.js
@@ -1,37 +1,37 @@
 export const stories = [
   {
     username: "kwangil_02",
-    profile: "/images/noti/id1profile.jpg",
+    profile: "/images/noti/id_1.jpg",
     story: "/images/noti/story_1.jpg",
     time: "16시간",
   },
   {
     username: "soomgo_job",
-    profile: "/images/noti/id2profile.jpg",
+    profile: "/images/noti/id_2.jpg",
     story: "/images/noti/story_2.jpeg",
     time: "21시간",
   },
   {
     username: "soomgo_job1",
-    profile: "/images/noti/id3profile.jpg",
+    profile: "/images/noti/id_3.jpg",
     story: "/images/noti/story_4.jpeg",
     time: "13시간",
   },
   {
     username: "soomgo_job2",
-    profile: "/images/noti/id1profile.jpg",
+    profile: "/images/noti/id_1.jpg",
     story: "/images/noti/story_3.jpeg",
     time: "5시간",
   },
   {
     username: "soomgo_job3",
-    profile: "/images/noti/id2profile.jpg",
+    profile: "/images/noti/id_2.jpg",
     story: "/images/noti/story_5.jpeg",
     time: "2시간",
   },
   {
     username: "soomgo_job4",
-    profile: "/images/noti/id3profile.jpg",
+    profile: "/images/noti/id_3.jpg",
     story: "/images/noti/story_6.jpeg",
     time: "30분",
   },
